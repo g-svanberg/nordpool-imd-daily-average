282fd9cc9768e639baa519ec4144e7d38387564d
Gunnar Svanberg <gunnar.svanberg@seb.se>
2024-09-05 00:34:09 +0200

    BugFix

--- modules/nordpool.py
+++ modules/nordpool.py
@@ -3,6 +3,7 @@ import datetime
 
 """Default areacode for class instantiation"""
 areacode = "SE3"
+currency = "SEK"
 
 
 class Prices:
@@ -10,5 +11,5 @@ class Prices:
     def __init__(self):
         pass
 
-    def Get_Prices_for_one_date(self, date: datetime) -> str:
+    def get_prices_for_one_date(self, date: datetime) -> str:
         pass
