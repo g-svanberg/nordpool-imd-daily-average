6876622f3b1bbbc4969ccf4413d8ee3be3549a92
Gunnar Svanberg <gunnar.svanberg@seb.se>
2024-09-05 00:05:32 +0200

    boilerplate1

---
+++ modules/nordpool.py
@@ -0,0 +1,14 @@
+import httpx
+import datetime
+
+"""Default areacode for class instantiation"""
+areacode = "SE3"
+
+
+class Prices:
+    # Constructor
+    def __init__(self):
+        pass
+
+    def Get_Prices_for_one_date(self, date: datetime) -> str:
+        pass
